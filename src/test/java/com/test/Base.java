4a40e50dc4ed3fecf7a06d48b808d4aa0a8f7305
Saravana129 <saravana.github@gmail.com>
2024-02-06 19:47:15 +0530

    first commit

---
+++ src/test/java/com/test/Base.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+	
+		public static WebDriver driver;
+
+		public static void browserLaunch(String url) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.get(url);
+
+		}
+
+		public static void maximize() {
+			driver.manage().window().maximize();
+
+		}
+		public static void getCurrentUrl() {
+			String currentUrl = driver.getCurrentUrl();
+			System.out.println(currentUrl);
+
+		}
+		public static void getTittle() {
+			String tittle = driver.getTitle();
+			System.out.println(tittle);
+
+		}
+	}
+
+
+
