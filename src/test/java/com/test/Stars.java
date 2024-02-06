7e05d4f92dff4092d8c8eafb0bd5ddb955deff7c
Saravana129 <saravana.github@gmail.com>
2024-02-06 20:12:50 +0530

    reverse stars

--- src/test/java/com/test/Stars.java
+++ src/test/java/com/test/Stars.java
@@ -3,20 +3,20 @@ package com.test;
 public class Stars {
 	
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j <= i; j++) {
-				System.out.print("*");
-				
-			}
-			System.out.println();
-		}
-		
-//		for (int i = 0; i >= 5; i--) {
-//			for (int j = 0; j >=i; j--) {
+//		for (int i = 0; i < 5; i++) {
+//			for (int j = 0; j <= i; j++) {
 //				System.out.print("*");
+//				
 //			}
 //			System.out.println();
 //		}
+		
+		for (int i = 1; i<= 5; i++) {
+			for (int j = 5; j >=i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
